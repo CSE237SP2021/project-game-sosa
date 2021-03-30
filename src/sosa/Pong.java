28b565b34580a87d58c09ff77a182d76d0387107
Sarah Forbes <sarah.forbes@wustl.edu>
2021-03-30 10:06:09 -0500

    Added constants and implemented KeyListener for keyboard movement.

--- src/sosa/Pong.java
+++ src/sosa/Pong.java
@@ -10,6 +10,6 @@ public class Pong{
 
     public static void main(String[] args){
         MyFrame gameFrame = new MyFrame();
-  
+
     }
 }
--- src/sosa/Racket1.java
+++ src/sosa/Racket1.java
@@ -5,18 +5,22 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Racket1{
-    int xVal;
-    int yVal;
-    int velocity;
-    Graphics graphic;
+public class Racket1 implements KeyListener {
+    private int xVal;
+    private int yVal;
+    private int velocity;
+    private Graphics graphic;
+    private final int UPPER_BOUNDS;
+    private final int LOWER_BOUNDS;
+    private final int SPEED;
 
-    public Racket1(){
+    public Racket1() {
         this.xVal = 20;
         this.yVal = 225;
-        this.velocity = 1;
-
-
+        this.velocity = 0;
+        UPPER_BOUNDS = 440;
+        LOWER_BOUNDS = 0;
+        SPEED = 1;
     }
     /*
     public void KeyPressed(KeyEvent e){
@@ -27,24 +31,47 @@ public class Racket1{
         }
     }
 */
-    public void KeyPressed(KeyEvent e){
-        System.out.println("how");
-    }
-    public int getXVal(){
+    
+    public int getXVal() {
         return xVal;
     }
 
-    public int getYVal(){
+    public int getYVal() {
         return yVal;
     }
 
+    private void boundaryCheck() {
+    	if (yVal >= UPPER_BOUNDS || yVal <= LOWER_BOUNDS) {
+    		velocity = 0;
+    	}
+    }
 
     public void move(){
-        if(this.yVal >= 440 || this.yVal <= 0){
-            this.velocity = -this.velocity;
-        }
-
-        this.yVal+=this.velocity;
+        boundaryCheck();
+        yVal += velocity;
     }
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			velocity = -SPEED;
+		}
+		else if (e.getKeyCode() == KeyEvent.VK_S) {
+			velocity = SPEED;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		velocity = 0;
+	}
 
 }
