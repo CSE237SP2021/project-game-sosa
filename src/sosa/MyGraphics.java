060da6a7a66c960bb6beb0d72de42b2ab04d0ac0
Arric Lucas <arriclucas@wustl.edu>
2021-04-13 21:46:30 -0500

    cleaned up some code

--- src/sosa/MyGraphics.java
+++ src/sosa/MyGraphics.java
@@ -7,9 +7,7 @@ import javax.swing.*;
 public class MyGraphics extends JPanel implements ActionListener {
     
     Timer timer = new Timer(4, this);
-    int racket2XVal  = 480;
-    int racket2YVal = 225;
-    int yVelocity = 1;
+
     int frameWidth = 500;
     int frameHeight = 500;
     Racket1 racket1;
